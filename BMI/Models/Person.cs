dcce1349365b007e04b33be05109485cd74ce68a
Maxim <maximum.cad@gmail.com>
2024-04-07 22:26:07 +0300

    Добавьте файлы проекта.

---
+++ BMI/Models/Person.cs
@@ -0,0 +1,38 @@
+﻿namespace Norbit.CalculateBMI.Models
+{
+    /// <summary>
+    /// Человек.
+    /// </summary>
+    internal class Person
+    {
+        /// <summary>
+        /// Вес.
+        /// </summary>
+        private readonly double Weight;
+
+        /// <summary>
+        /// Рост.
+        /// </summary>
+        private readonly double Height;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="weight">Вес.</param>
+        /// <param name="height">Рост.</param>
+        public Person(double weight, double height)
+        {
+            Weight = weight;
+            Height = height;
+        }
+
+        /// <summary>
+        /// Рассчет Индекса массы тела.
+        /// </summary>
+        /// <returns></returns>
+        public double CalculateBMI()
+        {
+            return Weight / (Math.Pow(Height, 2));
+        }
+    }
+}
---
+++ BMI/Program.cs
@@ -0,0 +1,67 @@
+﻿using Norbit.CalculateBMI.Models;
+
+namespace Norbit.CalculateBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weight = 0.0;
+            var height = 0.0;
+            
+            var isSuceessParsedWeight = false;
+            while (!isSuceessParsedWeight)
+            {
+                Console.WriteLine("Введите свой вес в кг.:");
+                isSuceessParsedWeight = double.TryParse(Console.ReadLine(), out weight);
+
+                if (!isSuceessParsedWeight)
+                    Console.WriteLine("Введено некорректное значение веса");
+                
+                if (weight > 600) // Максимальный вес человека на планете - Урибе Мануэль из Мексики 597 кг.
+                {
+                    Console.WriteLine("Невозможный вес");
+                    isSuceessParsedWeight = false;
+                }
+
+            }
+
+            var isSuceessParsedHeight = false;
+            while (!isSuceessParsedHeight)
+            {
+                Console.WriteLine("Введите свой рост в метрах:");
+                isSuceessParsedHeight = double.TryParse(Console.ReadLine(), out height);
+
+                if (!isSuceessParsedHeight)
+                    Console.WriteLine("Введено некорректное значение роста");
+
+                if (height > 2.60) // Максимальный рост человека на планете - Кёсен Султан из Турции 2.51 м. 
+                {
+                    Console.WriteLine("Невозможный рост");
+                    isSuceessParsedHeight = false;
+                }
+            }
+            
+            var person = new Person(weight, height);
+            var bmi = person.CalculateBMI();
+            var result = string.Empty;
+            
+            if (bmi < 18)
+            {
+                result = "Ниже нормального";
+            }
+            else if (bmi >= 18 && bmi < 25)
+            {
+                result = "Нормальный";
+            }
+            else
+            {
+                result = "Выше нормального";
+            }
+            
+            Console.WriteLine($"ИМТ = {Math.Round(bmi, 2)} - {result}");
+            Console.ReadKey();
+
+        }
+    }
+}
